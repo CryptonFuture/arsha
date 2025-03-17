d9f580b1dcacfc5e3e001669ef65dadbe5c64ebc
CryptonFuture <86903729+CryptonFuture@users.noreply.github.com>
2025-03-17 13:40:42 +0500

    firebase firestore database add

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -18,7 +18,7 @@
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
       if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!');
+        displayError(thisForm, 'please fill out all fields');
         return;
       }
       thisForm.querySelector('.loading').classList.add('d-block');
